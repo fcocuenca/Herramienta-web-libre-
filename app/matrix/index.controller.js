300247f56c93499afd2a750b644ac4263d51376a
fcocuenca <pakito_5_24@hotmail.com>
2017-07-07 21:18:00 +0200

    170517

--- app/matrix/index.controller.js
+++ app/matrix/index.controller.js
@@ -14,12 +14,13 @@
 			vm.matrix = null;
 
 			vm.name=null;
-			vm.selected=null;
+			vm.selected=[];
 			vm.prueba = [];
 
 			/*funciones*/
 			vm.avisar = avisar;
 			vm.comprobar = comprobar;
+			vm.eliminar =eliminar;
 
 
 			initSpec();			
@@ -45,25 +46,35 @@
 		    }
 
 		    function avisar(name, number, id){
-		    		console.log(name);
-		    		console.log(number);
-		    		console.log(id)
+		    	
 		    		var i=0;
 		    		
 		    		if(vm.selected[id] == true){
 		    			vm.prueba.push({'idRF': number, 'idCU': name, 'id': id});
 		    			i++;
+		    			console.log("añado"+id);
 		    		}
 
-	    			if(vm.selected[id] == false)
-	    			{
-	    				
-	    			}
-
+		    		if(vm.selected[id] == false)
+		    		{
+		    			for(i=0; i<vm.prueba.length; i++)
+		    			{
+			    			if(id == vm.prueba[i].id)
+			    			{
+			    				vm.eliminar(i);
+			    			}
+			    		}
+		    		}
 		    }
 
+		    function eliminar(id)
+		    {
+		    	vm.prueba.splice(id, 1);
+		    	console.log(vm.prueba);
+		   	}
+
 		    function comprobar(id){
-					
+				
 		    }
 	}
 })();
